8264c5e948e292477b91fc308d3e645ce82d90f3
K41 <136763358+K41-max@users.noreply.github.com>
2024-04-08 20:52:33 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,14 +35,11 @@ async def send_message():
                 if main_section and len(main_section) > 0:
                     current_number = int(main_section[0]['number'])
 
-                    if last_number is not None and current_number != last_number:
-                        info = main_section[0].get('info')  # info キーが存在しない場合は None を返す
-                        if info is None:  # infoがNoneの場合
-                            info = "null"
-
+                    if last_number is None or current_number != last_number:
+                        info = main_section[0].get('info', 'null')  # info キーが存在しない場合は 'null' を返す
                         await channel.send(f"\n\nnumber:{current_number}\nname:{main_section[0]['name']}\nmessage:{main_section[0]['message']}\ninfo:{info}\n")
 
-                    last_number = current_number
+                        last_number = current_number  # メッセージが送信されたら last_number を更新
 
             else:
                 print("No 'main' section found in the response.")
