6aa5f00f6fb39efc60e63bf53852937dbbad5945
K41 <136763358+K41-max@users.noreply.github.com>
2024-04-13 11:01:51 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,62 +5,54 @@ import requests
 import base64
 import json
 import time
-from flask import Flask
 
-# Flaskアプリを作成
-app = Flask(__name__)
+# クライアントを作成
+client = discord.Client(intents=discord.Intents.default())
 
-# Discordトークンを取得
 discord_token = os.environ['DISCORD_TOKEN']
 
 # メッセージを送信する関数
 async def send_message():
-    await app.client.wait_until_ready()  # ボットが完全に起動するまで待機
+    # メッセージを送信するチャンネルを取得
+    channel = client.get_channel(1217993869275168811)
 
     async def get_data(username, password):
+        # Basic認証のヘッダーを作成
         headers = {'Authorization': 'Basic ' + base64.b64encode(f"{username}:{password}".encode()).decode()}
         response = requests.get('https://yukibbs-server.onrender.com/bbs/admin', headers=headers)
         return response.json()
 
-    username = 'admin'
-    password = 'gafa074256Pass'
-    last_message_content = None
+    async def main():
+        username = 'admin'
+        password = 'gafa074256Pass'
+        last_message_content = None  # 直前のメッセージの内容を保持する変数を追加
 
-    while True:
-        data = await get_data(username, password)
+        while True:
+            data = await get_data(username, password)
 
-        if 'main' in data:
-            main_section = data['main']
-            if main_section and len(main_section) > 0:
-                message_content = f"number:{main_section[0]['number']}\nname:{main_section[0]['name']}\nmessage:{main_section[0]['message']}\ninfo:{main_section[0].get('info', 'null')}\n"
+            if 'main' in data:
+                main_section = data['main']
+                if main_section and len(main_section) > 0:
+                    message_content = f"number:{main_section[0]['number']}\nname:{main_section[0]['name']}\nmessage:{main_section[0]['message']}\ninfo:{main_section[0].get('info', 'null')}\n"
 
-                if message_content != last_message_content:
-                    await app.client.get_channel(1217993869275168811).send(message_content)
-                    last_message_content = message_content
+                    if message_content != last_message_content:
+                        await channel.send(message_content)
+                        last_message_content = message_content  # 送信したメッセージの内容を更新
 
-        else:
-            print("No 'main' section found in the response.")
+            else:
+                print("No 'main' section found in the response.")
 
-        await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
-# Flaskアプリのルートエンドポイント
-@app.route('/', methods=['GET'])
-def run_bot():
-    if not app.client.is_ready():
-        asyncio.create_task(send_message())  # ボットを非同期で起動
-        return 'Bot is starting!'
+    await main()
 
-    return 'Bot is already running!'
+# ボットが起動したときのイベントハンドラ
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-# Discordボットのクラスを定義
-class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.loop.create_task(send_message())  # ボットを起動
+    # メッセージを送信するタスクを開始
+    await send_message()
 
-app.client = MyClient()  # DiscordボットのインスタンスをFlaskアプリに割り当て
-
-# Flaskアプリの起動
-if __name__ == '__main__':
-    app.client.run(discord_token)  # Discordボットを起動
-    app.run(debug=True)
+# Discordに接続する
+client.run(discord_token)
