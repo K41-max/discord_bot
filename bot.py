e49b82fd35d92170c52662fe67b33a6fb9aecbb6
K41 <136763358+K41-max@users.noreply.github.com>
2024-04-13 11:33:27 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,41 @@ import requests
 import base64
 import json
 import time
+import sys
+import psutil
 
 # クライアントを作成
 client = discord.Client(intents=discord.Intents.default())
 
 discord_token = os.environ['DISCORD_TOKEN']
+
+# シングルトンのためのロックファイル
+LOCK_FILE_PATH = "/tmp/discord_bot.lock"
+
+# ロックファイルを作成してロックを取得する
+def acquire_lock():
+    try:
+        # 既にロックファイルが存在する場合は別のプロセスが起動している可能性があるので終了する
+        if os.path.exists(LOCK_FILE_PATH):
+            print("Another instance is already running. Exiting.")
+            sys.exit(1)
+        
+        # ロックファイルを作成してロックを取得する
+        with open(LOCK_FILE_PATH, "w") as lockfile:
+            lockfile.write(str(os.getpid()))
+    except Exception as e:
+        print("Error acquiring lock:", e)
+        sys.exit(1)
+
+# ロックを解放してロックファイルを削除する
+def release_lock():
+    try:
+        os.remove(LOCK_FILE_PATH)
+    except Exception as e:
+        print("Error releasing lock:", e)
+
+# シングルトンのためのロックを取得する
+acquire_lock()
 
 # メッセージを送信する関数
 async def send_message():
@@ -25,7 +55,7 @@ async def send_message():
     async def main():
         username = 'admin'
         password = 'gafa074256Pass'
-        last_message_content = None  # 直前のメッセージの内容を保持する変数を追加
+        last_number = None
 
         while True:
             data = await get_data(username, password)
@@ -33,11 +63,16 @@ async def send_message():
             if 'main' in data:
                 main_section = data['main']
                 if main_section and len(main_section) > 0:
-                    message_content = f"number:{main_section[0]['number']}\nname:{main_section[0]['name']}\nmessage:{main_section[0]['message']}\ninfo:{main_section[0].get('info', 'null')}\n"
+                    current_number = int(main_section[0]['number'])
 
-                    if message_content != last_message_content:
-                        await channel.send(message_content)
-                        last_message_content = message_content  # 送信したメッセージの内容を更新
+                    if last_number is not None and current_number == last_number:
+                        await asyncio.sleep(1)
+                        continue  # 数字が変わらない場合はループを続行してデータを再取得
+
+                    info = main_section[0].get('info', 'null')  # info キーが存在しない場合は 'null' を返す
+                    await channel.send(f"\n\nnumber:{current_number}\nname:{main_section[0]['name']}\nmessage:{main_section[0]['message']}\ninfo:{info}\n")
+
+                    last_number = current_number  # メッセージが送信されたら last_number を更新
 
             else:
                 print("No 'main' section found in the response.")
@@ -51,8 +86,8 @@ async def send_message():
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-    # メッセージを送信するタスクを開始
-    await send_message()
+    # シングルトンのためのロックを解放する
+    release_lock()
 
 # Discordに接続する
 client.run(discord_token)
